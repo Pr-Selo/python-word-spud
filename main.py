0713d58c7fd799b50710d4d31a746ba66ee668a5
Pr. Selo <tmcmanus222@gmail.com>
2025-09-08 17:53:59 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ scores = []
 words = ["deal","power","shape","eaten","drank","apple","grind","it\'s"]
 points = -1
 players = int(input('player count? (2 to 8)'))
-rounds = round(16/players)
 word_chain = ''
+last_word = words[randint(0,7)]
 
 def clear_console():
  os.system('cls' if os.name == 'nt' else 'clear')
@@ -18,31 +18,36 @@ def fancy_exit(reason):
 
 if players < 2 or players > 8:
  fancy_exit('invaild player count')
- 
+rounds = round(16/players)
+
 for i in range (players):
+ clear_console()
  scores.append(0)
  names.append(input(f'player {i+1}\'s name?'))
- clear_console()
 
 for i in range (players*rounds):
- last_word = word if points > -1 else words[randint(0,7)]
- word_chain.join(last_word)
+ clear_console()
+ word_chain = word_chain + last_word
  player_up = i % players
  print(f'{names[player_up]} is up!')
  print(f'round {i//players + 1} out of {rounds}')
  word = input(last_word)
+ points = 0
  for j in range (players):
   if not(j == player_up):
    like = input(f'{names[j]}, do you like it? (y/n):')
    points =+ (1 if like == 'y' else -1)
  scores[player_up] += points
- clear_console()
+ last_word = word if points > -1 else words[randint(0,7)]
 
+clear_console()
+print('winners:')
 highest = -99
 for j in range (players):
  highest = scores[j] if scores[j] > highest else highest
 for j in range (players):
- print(names[j] if scores[j] == highest else '')
+ if scores[j] == highest:
+  print(names[j])
 
-print(f'highest score: {highest}')
-fancy_exit(f'the word chain you all made:\n{word_chain}')
+print(f'\nhighest score: {highest}')
+fancy_exit(f'the word chain you all made:\n\n{word_chain}')
