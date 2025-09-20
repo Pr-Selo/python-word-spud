763b4d2abe24ccaf3f93c2cef9a1ed3ad660a48e
Pr. Selo <tmcmanus222@gmail.com>
2025-09-19 21:42:33 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-from random import randint
+from random import choice
 
 names = []
 scores = []
 words = ["deal","power","shape","eaten","drank","apple","grind","it\'s"]
-players = int(input('player count? (2 to 8)'))
+j = int(input('player count? (2 to 8)'))
 word_chain = ''
-last_word = words[randint(0,7)]
+last_word = choice(words)
 
 def clear_console():
  os.system('cls' if os.name == 'nt' else 'clear')
@@ -15,38 +15,36 @@ def fancy_exit(reason):
  input(f'{reason}\npress enter to exit the program.')
  exit()
 
-if players < 2 or players > 8:
+if j < 2 or j > 8:
  fancy_exit('invaild player count')
-rounds = round(16/players)
+rounds = round(16/j)
 
-for i in range (players):
+for i in range(j):
  clear_console()
  scores.append(0)
  names.append(input(f'player {i+1}\'s name?'))
 
-for i in range (players*rounds):
+for i in range(len(names)*rounds):
  points = 0
  clear_console()
- word_chain = word_chain + last_word
- player_up = i % players
+ word_chain.join(last_word)
+ player_up = i % len(names)
  print(f'{names[player_up]} is up!')
- print(f'round {i//players + 1} out of {rounds}')
+ print(f'round {i//len(names)+1} out of {rounds}')
  word = input(last_word)
- for j in range (players):
+ for j in range (len(names)):
   if not(j == player_up):
    like = input(f'{names[j]}, do you like it? (y/n):')
-   points += int(1 if like == 'y' else -1)
+   points += 1 if like == 'y' else -1
  scores[player_up] += points
- last_word = word if points > -1 else words[randint(0,7)]
+ last_word = word if points > -1 else choice(words)
 
 clear_console()
 print('winners:')
-highest = -99
-for j in range (players):
- highest = scores[j] if scores[j] > highest else highest
-for j in range (players):
- if scores[j] == highest:
-  print(names[j])
 
+highest = max(scores)
+for i in range (len(names)):
+ if scores[i] == highest:
+  print(names[i])
 print(f'\nhighest score: {highest}')
-fancy_exit(f'the word chain you all made:\n\n{word_chain}')
+fancy_exit(f'your word chain:\n\n{word_chain}')
